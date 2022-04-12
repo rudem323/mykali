8417236ccc81f508c3fd84069c4b0157966a2a3a
rudem323 <rudem323@gmail.com>
2022-04-12 16:41:27 -0400

    moddified_cloudenum

--- scripts/install-cloud_enum.sh
+++ scripts/install-cloud_enum.sh
@@ -5,17 +5,6 @@ trap "echo -e \"\033[1;31m[!] \e[0m Script error occured.\"" ERR
 GREEN="\033[1;32m"
 ENDCOLOR="\e[0m"
 
-# === Exit without proceeding if run in WSL ===
-if [ -f /mnt/c/Windows/System32/wsl.exe ]; then
-    exit 0
-fi
-
-# === Exit without proceeding if run on a non-Kali distro ===
-if ! grep -Fxvq "kali-rolling" /etc/debian_version; then
-    exit 0
-fi
-
-# Check if cloud_enum is installed
 if [ ! -d "/opt/cloud_enum" ]
 then
     echo -ne $GREEN"[+] "$ENDCOLOR; echo "Installing cloud_enum"
