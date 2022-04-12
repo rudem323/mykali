a1060152d1241b56f497c5c7c93fec48fd9e1542
rudem323 <47099672+rudem323@users.noreply.github.com>
2022-04-12 12:24:19 -0400

    added xclip to packages

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ if 'Kali' in release:
                            'apt-transport-https', 'curl', 'filezilla', 'meld', 'ncat', 'net-tools',
                            'tmux', 'bash-completion', 'ieee-data', 'python3-netaddr',
                            'ruby-full', 'powercat', 'cewl', 'nbtscan', 'tree', 'upx-ucl',
-                           'exe2hexbat', 'shellter', 'grc', 'flameshot', 'stow']
+                           'exe2hexbat', 'shellter', 'grc', 'flameshot', 'stow', 'xclip']
 
     # These kali packages will be removed
     packages_to_remove = []
